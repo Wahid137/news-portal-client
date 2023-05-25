693d0a7a6e30a6b4b0e4fbfa773704b92270971b
wahid-ahmed <wahidahmedshanto@gmail.com>
2023-05-26 01:51:00 +0600

    firebase and vercel deploy

--- .history/src/Pages/Shared/Footer/Footer_20230526010708.js
+++ .history/src/Pages/Shared/Footer/Footer_20230526010708.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <div>
-            <p className='text-center'>Copyright Wahid Ahmed Shanto 2023</p>
+            <p>Copyright Wahid Ahmed Shanto 2023</p>
         </div>
     );
 };
